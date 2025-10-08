6c554075e46408f7129589743dbc10cbe47583f9
Dimple-Saini06 <dimple13619saini@gmail.com>
2025-10-08 18:51:26 +0530

    Arrays

--- javascript/practice.js
+++ javascript/practice.js
@@ -1,10 +1,41 @@
 //que
 
-let msg = "help!!";
+/*let msg = "help!!";
 console.log(msg.trim().toUpperCase());
 
 let name = "ApnaCollege";
 
 console.log(name.replace("Apna", "Our"));
 console.log(name.slice(4, 9));
-console.log(name.indexOf("na"));
+console.log(name.indexOf("na"));*/
+
+
+//ARRAY
+let arr = ["dimple", "kushal", "muskan", "chottuu"];
+console.log(typeof arr);
+arr[3] = "chotu";
+console.log(arr);
+
+
+let frut = ['apple' , 'bn'];
+frut[5] = 'litchi';
+
+console.log(frut)
+
+let month = ["jan", "july", "march", "augest"];
+
+console.log(month);
+
+month.shift();
+
+console.log(month);
+
+month[1] = "june";
+
+console.log(month);
+
+month.indexOf("june");
+
+month.includes("may");
+
+month.includes("augest");

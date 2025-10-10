c58cea21f653da9d5d89d65a8b8dbd7b6102a072
Dimple-Saini06 <dimple13619saini@gmail.com>
2025-10-10 08:45:28 +0530

    Array Topics Completed

--- javascript/practice.js
+++ javascript/practice.js
@@ -11,7 +11,7 @@ console.log(name.indexOf("na"));*/
 
 
 //ARRAY
-let arr = ["dimple", "kushal", "muskan", "chottuu"];
+/*let arr = ["dimple", "kushal", "muskan", "chottuu"];
 console.log(typeof arr);
 arr[3] = "chotu";
 console.log(arr);
@@ -38,4 +38,29 @@ month.indexOf("june");
 
 month.includes("may");
 
-month.includes("augest");
+month.includes("augest");*/
+
+
+/*let car = ["XVM", "BMW", "baleno", "toyoto"];
+console.log(car.slice(1,3));
+console.log(car);*/
+
+//que
+/*let mnt = ['jan', 'july', 'march', 'august'];
+console.log(mnt.splice(0,1));
+let chng= mnt.splice(1,0, 'june');
+console.log(mnt);
+
+let code = ['c', 'c++', 'html', 'javascript', 'python', 'java', 'c#', 'sql'];
+console.log(code.reverse());
+console.log("the index of javascript is: ", code.indexOf('javascript'));*/
+
+
+//GAME TICTOC
+let game = [['X', null, 'O'], [null, 'X', null], ['O', null, 'X']];
+
+//que
+
+let num = [1,2,3,4,5];
+console.log(num.slice(0,3));
+console.log(num.slice(-3));

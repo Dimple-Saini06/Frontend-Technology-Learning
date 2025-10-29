46637d9c5246a132a8923bc58af6b9b83cb1f04a
Dimple-Saini06 <dimple13619saini@gmail.com>
2025-10-29 19:07:45 +0530

    LEARN ARRAY METHODS
    I learned array methods every, forEach, map, filter, some, reduce..

--- javascript/arrayMethod.js
+++ javascript/arrayMethod.js
@@ -17,7 +17,7 @@ arr.forEach(function(el){
 });*/
 
 
-let student = [
+/*let student = [
     {
         name:"Aman",
         marks:95,
@@ -37,4 +37,78 @@ let gpa = student.map((el) => {
 });
 
 
-console.log(gpa);
+console.log(gpa);*/
+
+
+// let arr = [2,4,6,1];
+
+/*let newArr = arr.filter(function(el) {
+    return el%2==0;
+});
+
+console.log(newArr);*/
+
+// let newarr = arr.some((el) => (el%2==0));
+
+/*let arr = [2,3,4,6,1,7,5,8,9,0];
+
+let ans = arr.reduce((max, el) => {
+    if(max  < el){
+        console.log(el);
+        return el;
+    }
+    else{
+        return max;
+    }
+})*/
+
+
+/*let arr = [10,50,20];
+
+let ans = arr.every(function(el) {
+    return el%10 == 0;
+});
+
+console.log(ans);*/
+
+
+/*let arr = [1,2,3,5,0,4,8];
+
+let minVal = arr.reduce((min, el) => {
+    if(min > el){
+        return el;
+    }
+    else{
+        return min;
+    }
+    
+});
+
+
+console.log(minVal);*/
+
+
+/*function sum(a, b=8){
+    return a+b;
+}
+
+console.log(sum(2));*/
+
+
+// let arr = [2,19,3,4,10,2,4,9,1];
+// let min = arr[0];
+// for(let i=1;i<arr.length;i++){
+//     min = Math.min(min,arr[i]);
+    
+// }
+// console.log(min);
+/*let newarr = [...arr];
+console.log(arr);*/
+
+
+const info = {
+    email : "xyz.gmail.com",
+    password : 123
+};
+
+const dataCopy = {...info, id:101};

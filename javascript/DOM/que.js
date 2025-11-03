cb4585dc1dda6bdce83b6e0f2e694a3dddbf8dd9
Dimple-Saini06 <dimple13619saini@gmail.com>
2025-11-03 18:00:38 +0530

    LEARN DOM
    I solved some questions related to DOM..

--- javascript/DOM/que.js
+++ javascript/DOM/que.js
@@ -1,18 +1,23 @@
-let para = document.createElement('p');
+/*let para = document.createElement('p');
 para.innerText = "Hey I'm red!";
 let body = document.querySelector('body');
 body.append(para);
-para.style.color = 'red';
+// para.style.color = 'red';
+para.classList.toggle('red');
+
 
 let heading = document.createElement('h3');
 heading.innerText = "I'm a blue h3!";
 body.append(heading);
-heading.style.color = 'blue';
+// heading.style.color = 'blue';
+heading.classList.toggle('blue');
+
 
 let div = document.createElement('div');
 body.append(div);
-div.style.backgroundColor = 'pink';
-div.style.border = '2px solid black';
+// div.style.backgroundColor = 'pink';
+// div.style.border = '2px solid black';
+
 
 let heading1 = document.createElement('h1');
 heading1.innerText = "I'm in a div";
@@ -21,4 +26,30 @@ div.append(heading1);
 let para1 = document.createElement('p');
 para1.innerText = "ME TOO!";
 div.append(para1);
+
+div.classList.toggle('divSty');*/
+
+
+/*let input = document.createElement('prompt');
+let button = document.createElement('button');
+button.innerText = "Click me";
+document.querySelector("body").append(button);
+document.querySelector("body").append(input);
+button.setAttribute('id', 'btn');
+console.dir(document.querySelector('button').getAttribute('id'));
+console.dir(document.querySelector('#btn'));
+button.classList.toggle('button');*/
+
+/*let p = document.createElement('p');
+p.innerHTML = "Apna College <b>Delta</b> Practice"
+document.querySelector('body').append(p);*/
+
+
+/*let h = document.createElement('h1');
+h.innerHTML = "DOM Practice";
+document.querySelector('body').append(h);
+h.classList.toggle('color');*/
+
+
+
 

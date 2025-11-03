9387e458cb09203ba2aa8cdab88e30942614ba07
Dimple-Saini06 <dimple13619saini@gmail.com>
2025-11-03 10:37:55 +0530

    LEARN DOM
    I have learned DOM manipulation with style, classList, and attributes.. learned query selector...

---
+++ javascript/DOM/app.js
@@ -0,0 +1,29 @@
+/*let small = document.getElementsByClassName("oldImg");
+for(let i=0;i<small.length;i++){
+    small[i].src = "spiderman_img.png";
+    console.log(`value ${i}  is changed..`);
+}*/
+
+/*let selector = console.dir(document.querySelector("h1"));
+let classSelect = console.dir(document.querySelector(".oldImg"));
+let idSelect = console.dir(document.querySelector("#mainImg"));
+let allSelect = console.dir(document.querySelectorAll("p"));
+console.dir(document.querySelectorAll(".box a"));*/
+
+/*let para = document.querySelector("h1");
+console.dir(para.innerText);*/
+
+
+// let image = document.querySelector("img");
+/*console.dir(document.querySelector("img").getAttribute("id"));
+console.dir(document.querySelector("img").setAttribute("id", "setImg"));*/
+
+
+/*let change = document.querySelectorAll(".box a");
+for(let i=0;i<change.length;i++){
+    change[i].style.backgroundColor = "yellow";
+}*/
+
+
+let heading = document.querySelector('h1');
+heading.classList.add("green");

38bbe12d0c65f5a5f5cadc7b5c69822d80f7cc2d
Dimple-Saini06 <dimple13619saini@gmail.com>
2025-11-04 18:11:49 +0530

    LEARN DOM
    In DOM I learned different types of EVENTS..

--- javascript/DOM/app.js
+++ javascript/DOM/app.js
@@ -49,3 +49,78 @@ para.insertAdjacentElement('afterend', btn);*/
 // document.querySelector('body').removeChild(btn);
 // btn.remove();
 
+/*let btn = document.querySelector("button");
+btn.onclick = function (){
+    console.log("print");
+}*/
+
+// let btns = document.querySelectorAll("button");
+/*for(btn of btns){
+    btn.onclick = sayHello;
+
+    btn.onmouseenter = function(){
+        console.log("YOU USE mouseenter");
+    }
+}
+function sayHello(){
+        console.log("YOU USE onclick");
+    }
+}*/
+
+/*for(btn of btns){
+    btn.addEventListener("click", function(){
+        console.log("YOU USE EVENT LISTENER");
+    })
+
+    btn.addEventListener("click", function(){
+        alert("YOU USE EVENT");
+    })
+
+}*/
+
+/*let box = document.querySelector(".box");
+
+box.addEventListener("mouseenter", function() {
+    console.log("Hover The Div");
+})*/
+
+// let btn = document.querySelector("button");
+/*btn.addEventListener("click", function() {
+    console.log(this);
+    console.dir(this);
+    this.style.background = "blue";
+})*/
+
+
+/*btn.addEventListener("mouseenter", function() {
+    console.log("button clicked");
+    console.log(event);
+})*/
+
+/*let inp = document.querySelector("input");
+inp.addEventListener("keydown", function(event) {
+    console.log("key presed");
+})*/
+
+
+let form = document.querySelector("form");
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    // let user = document.querySelector("#user");
+    // let pass = document.querySelector("#pass");
+    /*let user = form.elements[0];
+    let pass = form.elements[1];
+    console.log(user.value);
+    console.log(pass.value);*/
+});
+
+let user = document.querySelector("#user");
+user.addEventListener("change", function(event){
+    console.log("input event");
+    console.log("final event", user.value);
+});
+
+user.addEventListener("input", function(event){
+    console.log("input event1");
+    console.log("final event1", user.value);
+});

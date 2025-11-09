20907bd4b2209c8aea0b3384b946f256e006bd7e
Dimple-Saini06 <dimple13619saini@gmail.com>
2025-11-09 20:25:45 +0530

    COMPLETED MINI GAME
    This is SIMON SAYS GAME.. in this Game I used all the JavaScript concepts which I have already learned....

--- javascript/Mini-Project/app.js
+++ javascript/Mini-Project/app.js
@@ -5,12 +5,12 @@ let btns = ["red", "yellow", "green", "purple"];
 let started = false;
 let level = 0;
 
-let button = document.querySelectorAll(".btn");
+let allBtn = document.querySelectorAll(".btn");
 let h2 = document.querySelector("h2");
 
 document.addEventListener("keypress", function() {
     if(started == false){
-        console.log("true")
+        console.log("game started")
         started = true; 
 
         levelUp();
@@ -32,24 +32,51 @@ function userFlash(btn){
 }
 
 function levelUp(){
+    userSeq = [];
     level++;
     h2.innerText  = `Level ${level}`;
 
     let ranIdx = Math.floor(Math.random() * 4);
     let ranColor = btns[ranIdx];
     let ranBtn = document.querySelector(`.${ranColor}`);
-    console.log(ranIdx);
-    console.log(ranColor);
-    console.log(ranBtn);
+    gameSeq.push(ranColor);
+    console.log(gameSeq);
     gameFlash(ranBtn);
 }
 
-/*function btnPress() {
-    
-    userFlash(this);
+
+function checkAns(idx) {
+    if(userSeq[idx] === gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp, 1000);
+        }
+    }
+    else{
+        h2.innerHTML = `GAME OVER! Your Score was <b>${level}<b> <br>Press Any Key to Start.`
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 250);
+        reStart();
+    }
 }
 
-for(btns of button){
-    btns.addEventListener("click", btnPress);
-}*/
+function btnPress() {
+    let btn = this;
+    userFlash(btn);
+    let userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
 
+    checkAns(userSeq.length-1);
+}
+
+for (button of allBtn){
+    button.addEventListener("click", btnPress);
+}
+
+function reStart() {
+    started = false;
+    userSeq = [];
+    gameSeq = [];
+    level = 0;
+}

1bce84baf79066ecf8e76c1c838f9110d6f9ecc1
Dimple-Saini06 <dimple13619saini@gmail.com>
2025-11-11 22:24:25 +0530

    LEARN CALL STACK AND CALLBACK HELL

---
+++ javascript/class-11/app.js
@@ -0,0 +1,27 @@
+/*let h1 = document.querySelector("h1");
+
+function colorChange(color, delay, nextcolorChange){
+    setTimeout(() => {
+        h1.style.color = color;
+        nextcolorChange();
+    }, delay);
+}
+
+colorChange("red", 1000, () =>{
+    colorChange("green", 1000, () =>{
+        colorChange("blue", 1000, () =>{
+            colorChange("yellow", 1000, () =>{
+                colorChange("purple", 1000, () =>{
+                })
+            })
+        })
+    })
+})*/
+
+
+function saveToDb(data, success, failure){
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if(internetSpeed > 4){
+        
+    }
+}

3cf34f8508420ea2203c570191ba67202029d175
Dimple-Saini06 <dimple13619saini@gmail.com>
2025-11-17 14:42:41 +0530

    LEARN APIs

--- javascript/class-12/app.js
+++ javascript/class-12/app.js
@@ -12,7 +12,7 @@ let std = {
 let jsTojson = JSON.stringify(std);
 console.log(jsTojson);*/
 
-let url = "https://catfact.ninja/fact";
+// let url = "https://catfact.ninja/fact";
 
 /* fetch(url)
    .then((res) => {
@@ -42,13 +42,67 @@ let url = "https://catfact.ninja/fact";
         console.log(err);
     });*/
 
-async function getvalue(){
-    let res1 = fetch(url);
-    let data = (await res1.json());
-
-
+/*async function getvalue(){
+    let res1 = await fetch(url);
+    let data = await (res1.json());
     console.log(data.fact);
-}
+}*/
+
+/*async function getvalue() {
+    let res = await axios(url);
+    console.log(res);
+    console.log(res.data.fact);
+}*/
 
 
-console.log("This is after the fetch call");
+// console.log("This is after the fetch call");
+
+// let btn = document.querySelector("button");
+/*let p = document.querySelector("#fact");
+
+btn.addEventListener("click", async function() {
+    let result = await getvalue();
+    p.innerText = result;
+});
+
+async function getvalue() {
+    try{
+        let res = await axios(url);
+        return res.data.fact;
+    }
+    catch(e) {
+        return "error occured";
+    }
+}*/
+
+/*let url2 = "https://dog.ceo/api/breeds/image/random";
+let img = document.querySelector("#result");
+
+btn.addEventListener("click", async function() {
+    let result = await getvalue();
+    img.src = result;
+});
+async function getvalue() {
+    try{
+        let res = await axios(url2);
+        return res.data.message;
+    }
+    catch(e) {
+        return "NO image found";
+    }
+}*/
+
+
+/*const url = "https://icanhazdadjoke.com/";
+async function getJoke() {
+    try{
+        const config = {headers : {Accept : "application/json"}};
+        let res = await axios.get(url, config);
+        console.log(res.data.joke);
+    }
+
+    catch(err) {
+        console.log(err);
+    }
+}*/
+

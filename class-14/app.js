d26e5ff38ef11755f38b4f7fd10e4af0c998c38c
Dimple-Saini06 <dimple13619saini@gmail.com>
2026-02-13 14:05:14 +0530

    class-14

---
+++ class-14/app.js
@@ -0,0 +1,93 @@
+// console.log(1+3);
+// console.log(3**2);
+// let a = 5;
+// let b = '5';
+// console.log(a==b);
+// console.log(a===b);
+// let age = prompt("enter age");
+// age>=18?console.log("Adult"):console.log("not");
+
+//STRING
+// let s = "apna college";
+// console.log(s.slice(-9));
+// console.log(s.repeat(3).replace('l', '1'));
+
+//ARRAY
+// let arr = ['a', 'b','c'];
+// let s = 1;
+// let s1 = "1";
+// // let s2 = s.concat(s1);
+// arr[7] = 'd';
+// arr.unshift('e');
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+
+// const arr = [1,2,3];
+// let arr1 = [[2,4], [6,8]];
+// for(let a of arr1){
+//     for(let a1 of a){
+//         console.log(a1);
+//     }
+// }
+// let a = prompt("num");
+// a = parseInt(a);
+// for(let i=1;i<=10;i++){console.log(a * i);}
+
+
+//OBJECTS
+// let std = {
+//     // name : "dimple",
+//     // branch : "IT",
+//     // Rollno : 9
+//     aman : {
+//         grade : 'A+',
+//         city : 'Ajmer'
+//     },
+//     shardha : {
+//         grade : 'A',
+//         city : 'Jaipur'
+//     }
+// };
+
+// for(let keys in std){
+//     console.log("keys : " + keys + " value : " + std[keys]);
+// }
+
+// std.garima={grade:'A++', city:"Jodhpur"};
+
+
+//ARRAY OF OBJECT
+// let std = [
+//     {
+//         name : "dimple",
+//         grade : 'A+',
+//         city : 'Ajmer' 
+//     },
+
+//     {
+//         name : "dimple",
+//         grade : 'A+',
+//         city : 'Ajmer' 
+//     }
+// ];
+
+// let name = prompt("enter the name");
+// let age = prompt("enter the age");
+// let print = alert(`${name} is ${age} year old`);
+
+// let arr= [1,2,3,4,5];
+// let n = prompt("enter n");
+// for(let i=arr.length-1;i>=n;i--){
+//     console.log(arr[i]);
+// }
+
+
+let s = Math.floor(Math.random() * 5) + 11;
+
+let car = {
+    name : "bmw",
+    Model : "High",
+    color : "black"
+};
